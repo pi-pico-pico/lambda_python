086520e706d9baa8dec4a0115069f5dfe084a28f
pico <pico.pico.code@gmail.com>
2021-05-24 21:59:07 +0900

    内容修正

--- example4.py
+++ example4.py
@@ -1,1 +1,15 @@
+import json
+
 # イベント引数をログに書き出すプログラム
+def lambda_handler(event, context):
+    print(json.json.dumps(event, indent=4))
+    
+    # JSON形式の戻り値を設定する
+    return {
+        'statusCode' : 200,
+        'headers' : {
+            'content-type' : 'text/html'
+        },
+        'body' : '<html><body>OK</body></html>'
+    }
+
