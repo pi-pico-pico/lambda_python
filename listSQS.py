77dfdbde37eeea68d468d5ef5951c7b8749f3a38
pico <pico.pico.code@gmail.com>
2021-06-25 15:00:00 +0900

    ソース修正

--- listSQS.py
+++ listSQS.py
@@ -3,3 +3,12 @@ import urllib.parse
 import boto3
 from boto3.dynamodb.conditions import key, attr
 
+def lambda_handler(event, context):
+    # ①DynamoDBのmailaddressテーブルを操作するオブジェクト
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('mailaddress')
+
+    # ②SQSのキューを操作するオブジェクト
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName="mailsendqueue000")
+    
