63ebd5351eea63bbde070b5fde5b1a61d583684e
pico <pico.pico.code@gmail.com>
2021-07-13 23:10:24 +0900

    ソース追記

--- sendEmail.py
+++ sendEmail.py
@@ -1,3 +1,10 @@
 import json
 import boto3
 
+sqs = boto3.resource('sqs')
+s3 = boto3.resource('s3')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('mailaddress')
+
+client = boto3.client('ses', region_name='us-east-1')
+MAILFROM= ''
