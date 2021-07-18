814089fa6463f83b6efb8ec954b715c70acd4593
pico <pico.pico.code@gmail.com>
2021-07-18 21:00:27 +0900

    ソース追記

--- sendEmail.py
+++ sendEmail.py
@@ -28,4 +28,61 @@ def lambda_handler(event, context):
                 username = m.message_attributes.get('username').get('StringValue')
                 backetname = m.message_attributes.get('backetname').get('StringValue')
                 filename = m.message_attributes.get('filename').get('StringValue')
-                
+                print(backetname)
+                print(filename)
+                print(username)
+                print(email)
+
+                # S3バケットから本文を取得する
+                obj = s3.Object(backetname, filename)
+                response = obj.get()
+                maildata = response['Body'].read().decode('utef-8')
+                datas = maildata.split("\n", 3)
+                subject = datas[0]
+                body = datas[2]
+
+                # 送信済みでないことを確認し、また、送信済みに設定する
+                response = table.update_item(
+                    Key = {
+                        'email' : email
+                    },
+                    UpdateExpression = "set issend=:val",
+                    ExpressionAttributeValues = {
+                    ':val' : 1
+                    },
+                    returnValues = 'UPDATED_OLD'
+                )
+
+                if response['Attributes']['issend'] == 0:
+                    # メール送信
+                    response = client.send_email(
+                        Source=MAILFROM,
+                        ReplyToAddresses=[MAILFROM],
+                        Destination= {
+                            'ToAddresses' : [
+                                email
+                            ]
+                        },
+                        Message={
+                            'Subject': {
+                                'Data' : subject,
+                                'Charsest' : 'UTF-8'
+                            },
+                            'Body' : {
+                                'Text' : {
+                                    'Data' : body,
+                                    'Charset' : 'UTF-8'
+                            }
+                        }
+                    }
+                )
+                else:
+                    print("Resend Skip")
+
+                print("Send To" + email)
+            else:
+                print("Message None")
+
+            # キューから取り除く
+            m.delete()
+
