9a210d75d07dbdcfabb1fcaf867cbf35e16221f1
pico <pico.pico.code@gmail.com>
2021-07-28 22:53:26 +0900

    ソース追記

--- bounce.py
+++ bounce.py
@@ -9,3 +9,8 @@ def lambda_handler(event, context):
     for rec in event['Records']:
         # バウンスしたメールアドレスの取得
         message = rec['Sns']['Message']
+        data = json.loads(message)
+        bounce = data['bounce']['bounceRecipients']
+        for b in bounces:
+            email = b['emailAddress']
+            # haserrorを1に設定する
